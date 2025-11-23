74bb13f31da7c7c3efb9f4c386cd74c2238a46f6
Gary <gary9716@gmail.com>
2025-11-24 04:38:21 +0800

    fix

--- environments/staging/nautilus-enclave/variables.tf
+++ environments/staging/nautilus-enclave/variables.tf
@@ -7,7 +7,7 @@ variable "aws_profile" {
 variable "eif_version" {
   description = "Version/tag of the EIF file to deploy (e.g., commit SHA). Update this when deploying new enclave versions."
   type        = string
-  default     = "8d13afe"
+  default     = "acb68b5"
 }
 
 # Note: create_mtls_client_secret variable removed
